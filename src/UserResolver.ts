d2f8510f10c6b8a3a640077c4b73c7e75128e771
kthirakul <kwangthirakul@gmail.com>
2020-09-11 18:12:13 +0700

    ADD login

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,6 +1,13 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql'
-import { hash } from 'bcryptjs'
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from 'type-graphql'
+import { hash, compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 import { User } from './entity/User'
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -9,11 +16,37 @@ export class UserResolver {
     return 'hi!'
   }
 
+  // Get User
   @Query(() => [User])
   users() {
     return User.find()
   }
 
+  // Login
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      throw new Error('could not find user')
+    }
+
+    const valid = await compare(password, user.password)
+
+    if (!valid) {
+      throw new Error('bad password')
+    }
+
+    // login successful
+    return {
+      accessToken: sign({ userId: user.id }, 'secret', { expiresIn: '15m' }),
+    }
+  }
+
+  // Register
   @Mutation(() => Boolean)
   async register(
     @Arg('email') email: string,
