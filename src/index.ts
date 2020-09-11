9198bf93dc5918cb5fe3ae69d175ee9601486f53
kthirakul <kwangthirakul@gmail.com>
2020-09-11 19:56:40 +0700

    ADD cookie token

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,39 @@ import { buildSchema } from 'type-graphql'
 import { UserResolver } from './UserResolver'
 import { createConnection } from 'typeorm'
 import cookieParser from 'cookie-parser'
+import { verify } from 'jsonwebtoken'
+import { User } from './entity/User'
+import { createAccessToken } from './auth'
 
 const main = async () => {
   const app = express()
   app.get('/', (_req, res) => res.send('hello'))
   app.use(cookieParser())
 
-  app.post('/refresh_token', (req) => {
+  app.post('/refresh_token', async (req, res) => {
+    const token = req.cookies.jid
+
+    if (!token) {
+      return res.send({ ok: false, accessToken: '' })
+    }
     console.log(req.cookies)
+
+    let payload: any = null
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!)
+    } catch (error) {
+      console.log(error)
+      return res.send({ ok: false, accessToken: '' })
+    }
+
+    // token is valid and we can send back an access
+    const user = await User.findOne({ id: payload.userId })
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: '' })
+    }
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) })
   })
 
   await createConnection()
